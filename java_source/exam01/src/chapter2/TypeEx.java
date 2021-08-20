64c3d62b462125ecff046fc9b8b2e9c39eda9a47
YONGGYO LEE <yonggyo1981@gmail.com>
2021-08-20 15:17:57 +0900

    .

---
+++ java_source/exam01/src/chapter2/TypeEx.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class TypeEx {
+	public static void main(String[] args) {
+		var num1 = 10; // int  -> int num1 = 10;
+		var num2 = 3.14; // double num2 = 3.14;
+		var text = "ABCD";  // String ¹®ÀÚ¿­ String text = "ABCD";
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(text);
+		
+		//text = 10; String text = 10;
+		text = "ABCDEFG";
+	}
+}
