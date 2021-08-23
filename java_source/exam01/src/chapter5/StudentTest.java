2f419d76e3e8945f45130288f57592be6d48f3cb
YONGGYO LEE <yonggyo1981@gmail.com>
2021-08-23 18:13:34 +0900

    .

---
+++ java_source/exam01/src/chapter5/StudentTest.java
@@ -0,0 +1,24 @@
+package chapter5;
+
+//import chapter4.SwitchEx1;
+//import chapter4.*;
+
+// java.util.ArrayList 
+import java.util.*;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student = new Student();
+		System.out.println(student);
+		
+		/*
+		SwitchEx1 ex1 = new SwitchEx1();
+		System.out.println(ex1);
+		
+		IfEx1 ex2 = new IfEx1();
+		*/
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		
+		
+	}
+}
