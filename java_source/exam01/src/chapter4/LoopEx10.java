e3d24fdbc048a764c09e761c944759ad05c31fbb
YONGGYO LEE <yonggyo1981@gmail.com>
2021-08-23 13:22:01 +0900

    .

---
+++ java_source/exam01/src/chapter4/LoopEx10.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+public class LoopEx10 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 != 0) { // 3의 배수가 아닌 경우는 건너뛰기
+				continue; 
+			}
+			
+			System.out.println(i);
+		}
+	}
+}
---
+++ java_source/exam01/src/chapter4/LoopEx12.java
@@ -0,0 +1,18 @@
+package chapter4;
+
+public class LoopEx12 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 1000; i++) {
+			if (i % 2 == 0) { // 짝수
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		if ('A' < 'B') {
+			System.out.println("B가 더 크다");
+		} else {
+			System.out.println("A가 더 크다");
+		}
+	}
+}
