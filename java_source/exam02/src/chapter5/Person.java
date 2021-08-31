222747e60131834844aef169cfdecafdeb190f1b
YONGGYO LEE <yonggyo1981@gmail.com>
2021-08-31 13:22:26 +0900

    .

---
+++ java_source/exam02/src/chapter5/Person.java
@@ -0,0 +1,16 @@
+package chapter5;
+
+public class Person {
+	java.lang.String name;
+	int height;
+	double weight;
+	char gender;
+	boolean married;
+	
+	FamilyMember family;
+	java.util.ArrayList<String> list;
+	
+	public void showInfo() {
+		System.out.println("Person Å¬·¡½º");
+	}
+}
---
+++ java_source/exam02/src/chapter5/sub/Ex1.java
@@ -0,0 +1,10 @@
+package chapter5.sub;
+
+import chapter5.Person;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.showInfo();
+	}
+}
