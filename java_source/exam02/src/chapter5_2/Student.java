3dd561446e0865eeabe4da1d60f733d72f7f02c9
YONGGYO LEE <yonggyo1981@gmail.com>
2021-08-31 18:14:43 +0900

    .

--- java_source/exam02/src/chapter5_2/Student.java
+++ java_source/exam02/src/chapter5_2/Student.java
@@ -1,5 +1,18 @@
 package chapter5_2;
 
 public class Student {
-	static int serialNum; // 일련번호 // 정적(고정 - 공유)
+	static int serialNum; // 일련번호 // 정적(고정 - 공유) // 데이터 영역 
+	int studentID; // 멤버 변수 -> 힙 영역 
+	
+	void method() {
+		staticMethod();
+		serialNum = 10;
+		System.out.println("인스턴스 메서드");
+	
+	static void staticMethod() {
+		//method();
+		//studentID = 10;
+		System.out.println("정적 메서드");
+	}
+	
 }
--- java_source/exam02/src/chapter5_2/StudentTest.java
+++ java_source/exam02/src/chapter5_2/StudentTest.java
@@ -2,17 +2,28 @@ package chapter5_2;
 
 public class StudentTest {
 	public static void main(String[] args) {
-		Student s1 = new Student();
-		Student s2 = new Student();
+		//System.out.println(Student.serialNum);
 		
+		//Student s1 = new Student();
+		//Student s2 = new Student();
+		Student.staticMethod();
+		
+		/*
 		s1.serialNum = 1000;
-		
+		s1.studentID = 1000;
 		System.out.println("s1.serialNum : " + s1.serialNum);
 		System.out.println("s2.serialNum : " + s2.serialNum);
 		
+		System.out.println("StudentID --------");
+		System.out.println("s1.studentID : " + s1.studentID);
+		System.out.println("s2.studentID : " + s2.studentID);
+		
+		/*
 		s2.serialNum = 2000;
 		
 		System.out.println("s1.serialNum : " + s1.serialNum);
 		System.out.println("s2.serialNum : " + s2.serialNum);
+		*/
+		
 	}
 }
