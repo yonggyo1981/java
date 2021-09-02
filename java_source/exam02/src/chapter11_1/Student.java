49c5344774d76595f4b0fb7e337fa1490759a0ad
YONGGYO LEE <yonggyo1981@gmail.com>
2021-09-02 15:12:37 +0900

    .

--- java_source/exam02/src/chapter11_1/Student.java
+++ java_source/exam02/src/chapter11_1/Student.java
@@ -1,6 +1,7 @@
 package chapter11_1;
 
-public class Student {
+/** 빈 인터페이스 -> Cloneable -> 의사 표현 -> 마커 인터페이스 */
+public class Student implements Cloneable {
 	int studentID; // 학번
 	String name; // 이름 
 	
@@ -10,7 +11,27 @@ public class Student {
 	}
 	
 	@Override
+	public boolean equals(Object obj) {
+		// 비교하는 Student 인스턴의 학번이 같으면 논리적으로 동일
+		Student s = (Student)obj;
+		if (s.studentID == studentID) 
+			return true;
+		
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return studentID;
+	}
+
+	@Override
 	public String toString() {
 		return "학번 : " + studentID + ", 이름 : " + name;
+	}
+	
+	@Override 
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
 	}
 }
