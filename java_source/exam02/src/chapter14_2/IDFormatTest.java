1d13dbc241faea25f4877bcfb5ac355808605541
YONGGYO LEE <yonggyo1981@gmail.com>
2021-09-08 15:13:29 +0900

    .

--- java_source/exam02/src/chapter14_2/IDFormatTest.java
+++ java_source/exam02/src/chapter14_2/IDFormatTest.java
@@ -2,6 +2,8 @@ package chapter14_2;
 
 public class IDFormatTest {
 	private String userID;
+	private String password;
+	
 	
 	public String getUserID() {
 		return userID;
@@ -24,6 +26,31 @@ public class IDFormatTest {
 		this.userID = userID;
 	}
 	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) throws PasswordException {
+		/*
+		 * 1) 비밀번호가 null(O)
+			2) 문자열로만 이뤄진 경우 
+			3) 5자 이하인 경우
+		 */
+		if (password == null) {
+			throw new PasswordException("비밀번호가 null이면 안되요");
+		}
+		
+		if (password.matches("[a-zA-Z]+") && password.matches("[!@#$%^&*]+") && password.matches("[0-9]+")) {
+			throw new PasswordException("문자열 이외에 특수문자 또는 숫자도 함께 입력");
+		}
+		
+		if (password.length() <= 5) {
+			throw new PasswordException("비밀번호는 6자리 이상");
+		}
+		
+		this.password = password;
+	}
+	
 	public static void main(String[] args) {
 		IDFormatTest ex = new IDFormatTest();
 		// 아이디 null X, 자리수는 8~20
@@ -38,6 +65,21 @@ public class IDFormatTest {
 		try {
 			ex.setUserID(userID);
 		} catch (IDFormatException e) {
+			System.out.println(e);
+		}
+		
+		String password = null;
+		try {
+			 password = null;
+			ex.setPassword(password);
+		} catch (PasswordException e) {
+			System.out.println(e);
+		}
+		
+		try {
+			password = "abcd123!";
+			ex.setPassword(password);
+		} catch (PasswordException e) {
 			System.out.println(e);
 		}
 	}
--- java_source/exam02/src/chapter14_2/PasswordException.java
+++ java_source/exam02/src/chapter14_2/PasswordException.java
@@ -1,5 +1,7 @@
 package chapter14_2;
 
-public class PasswordException {
-
+public class PasswordException extends Exception {
+	public PasswordException(String message) {
+		super(message);
+	}
 }
