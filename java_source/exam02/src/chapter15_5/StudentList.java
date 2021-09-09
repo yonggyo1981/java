2443c9c85dd022981b8f7021288a377d664c6ac1
YONGGYO LEE <yonggyo1981@gmail.com>
2021-09-09 15:40:00 +0900

    .

--- java_source/exam02/src/chapter15_5/StudentList.java
+++ java_source/exam02/src/chapter15_5/StudentList.java
@@ -1,5 +1,23 @@
 package chapter15_5;
 
-public class StudentList {
+import java.util.ArrayList;
+import java.io.Serializable;
+
+public class StudentList implements Serializable {
 	
+	private ArrayList<Student> list;
+	
+	public StudentList() {
+		list = new ArrayList<Student>();
+	}
+	
+	public void addStudent(Student s) {
+		list.add(s);
+	}
+	
+	public void showList() {
+		for (Student s : list) {
+			System.out.println(s);
+		}
+	}
 }
--- java_source/exam02/src/chapter15_5/StudentTest.java
+++ java_source/exam02/src/chapter15_5/StudentTest.java
@@ -8,6 +8,22 @@ import java.io.ObjectOutputStream;
 
 public class StudentTest {
 	public static void main(String[] args) {
+		StudentList list1 = new StudentList();
+		
+		list1.addStudent(new Student(1001, "이름1", 30));
+		list1.addStudent(new Student(1002, "이름2", 40));
+		list1.addStudent(new Student(1003, "이름3", 20));
+		
+		try (FileOutputStream fos = new FileOutputStream("student.obj");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			FileInputStream fis = new FileInputStream("student.obj");	
+			ObjectInputStream ois = new ObjectInputStream(fis))  {
+			
+			oos.writeObject(list1);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
