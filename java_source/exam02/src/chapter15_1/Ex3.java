45a44e184abad07362a901d7eb1dbef369f1b984
YONGGYO LEE <yonggyo1981@gmail.com>
2021-09-10 12:22:07 +0900

    .

--- java_source/exam02/src/chapter15_1/Ex3.java
+++ java_source/exam02/src/chapter15_1/Ex3.java
@@ -1,15 +1,19 @@
 package chapter15_1;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Ex3 {
 	public static void main(String[] args) {
 		
 		
+		InputStreamReader isr = new InputStreamReader(System.in);
 		try {
 			System.out.print("문자를 입력하시오 : ");
+			
+			
 			while(true) {
-				int i = System.in.read();
+				int i = isr.read();
 				System.out.print((char)i);
 			}
 		} catch (IOException e) {
