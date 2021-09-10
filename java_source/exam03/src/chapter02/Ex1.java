ad35641d158e68a00d6e51ab550288f9588b3d5e
YONGGYO LEE <yonggyo1981@gmail.com>
2021-09-10 16:14:59 +0900

    .

---
+++ java_source/exam03/src/chapter02/Ex1.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		//float nun = 123.456F;
+		long num = 12341212121212122L;
+		// int num = 12341212121212121212 -> (long)
+
+	}
+}
