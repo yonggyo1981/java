a376b8d7d00cae4c5eeb04be5be2fc7da070205b
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-12 12:21:52 +0900

    .

--- javasource2/JavaEx2/src/chapter13/OuterClass2.java
+++ javasource2/JavaEx2/src/chapter13/OuterClass2.java
@@ -1,8 +1,25 @@
 package chapter13;
 
 public class OuterClass2 {
-	public InterA get() {
+	public InterA get(int num3) {
+		return (x, y) -> x + y + num3;
 		
 		
+		/*
+		return new InterA() {
+			public int sum(int num1, int num2) {
+				return num1 + num2 + num3;
+			}
+		};\
+		*/
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		//OuterClass2 out = new OuterClass2();
+		//InterA in = out.get(30);
+		//System.out.println(in.sum(10, 20));
 	}
 }
