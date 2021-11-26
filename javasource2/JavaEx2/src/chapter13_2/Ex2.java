977d3b7c5bb386021f8cc241adf80ffc800f5f0f
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-26 14:54:41 +0900

    .

--- javasource2/JavaEx2/src/chapter13_2/Ex2.java
+++ javasource2/JavaEx2/src/chapter13_2/Ex2.java
@@ -2,17 +2,32 @@ package chapter13_2;
 
 import java.util.*;
 
-interface PreInter {
-	<E> ArrayList<E> test(E el);
+interface PreInter<E> {
+	boolean test(E el, int index);
 }
 
 public class Ex2 {
 	public static void main(String[] args) {
+		int[] list = {0, 1,2,3,4,5,6,7,8,9,10};
+	
+		ArrayList<Integer> nums = filter(list, (el, i)-> {
+			if (el % 2 == 0) 
+				return true;
+			return false;
+		});
+		
+		System.out.println(nums)
 		
 	}
 	
-	public <E> ArrayList<E> filter(PreInter inter) {
+	public static ArrayList<Integer> filter(int[] list, PreInter<Integer> inter) {
+		ArrayList<Integer> data = new ArrayList<>();
+		for (int i = 0; i < list.length; i++) {
+			if (inter.test(list[i], i)) {
+				data.add(list[i]);
+			}
+		}
 		
-		return null;
+		return data;
 	}
 }
