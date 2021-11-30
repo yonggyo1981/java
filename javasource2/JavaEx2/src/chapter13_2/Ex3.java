84e6de2c09d5955fd8f4f7807364701bc8dfa637
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-30 09:10:59 +0900

    .'

--- javasource2/JavaEx2/src/chapter13_2/Ex3.java
+++ javasource2/JavaEx2/src/chapter13_2/Ex3.java
@@ -6,25 +6,21 @@ interface PreInter2<T> {
 	boolean test(T el, int index);
 }
 
-public class Ex3 {
+public class Ex3 {	
 	public static void main(String[] args) {
-		Ex3 ex3 = new Ex3();
 		ArrayList<String> list = new ArrayList<>();
 		list.add("이름1");
 		list.add("이름2");
 		list.add("이름3");
-		list.add("이름4");
 		
-		ArrayList<String> result = ex3.<String>filter(list, (el, i) -> {
+		ArrayList<String> newList = filter(list, (el, i) -> {
 			return true;
 		});
-		
-		System.out.println(result);
+		System.out.println(newList);
 	}
 	
-
-	public static <T> ArrayList<T> filter(List<T> list, PreInter2<T> pre) {
-		ArrayList<T> list2 = new ArrayList<>();
+	public static ArrayList<String> filter(List<String> list, PreInter2<String> pre) {
+		ArrayList<String> list2 = new ArrayList<>();
 		for (int i = 0; i < list.size(); i++) {
 			if (pre.test(list.get(i), i)) {
 				list2.add(list.get(i));
---
+++ javasource2/JavaEx2/src/chapter13_2/Ex4.java
@@ -0,0 +1,5 @@
+package chapter13_2;
+
+public class Ex4 {
+	
+}
